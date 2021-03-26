680b9c63285e77f1cbeb2728923051b9bfc8f18b
lucianogatti <81379031+lucianogatti@users.noreply.github.com>
2021-03-25 23:32:23 -0300

    estuve trabajando en prueba 1

--- C-utn/Prueba1/Main.c
+++ C-utn/Prueba1/Main.c
@@ -2,7 +2,25 @@
 
 int main()
 {
-	printf("Hola Mundo\n");
-	system("Pause");
+	int a, b, c;
+
+	printf("Ingrese el primer numero: ");
+	scanf_s("%i", & a);
+
+	printf("Ingrese el segundo numero: ");
+	scanf_s("%i", & b);
+	
+	printf("Ingrese el tercer numero: ");
+	scanf_s("%i", & c);
+
+	int suma = a + b + c;
+	int resta = a - b - c;
+	double division =  a / b ;
+	division = division / c;
+	int multiplicaion = a * b * c;
+
+	printf("El resultado de la suma es: %i,\nEl resultado de la resta es: %i\nEl resultado de la division es: %f\nEl resultado de la multiplicaión es: %i.", suma, resta, division, multiplicaion);
+
 	return 0;
+
 }
